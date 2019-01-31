2ff8201573091ed0984855f24a07f9e432d5473c
Travis Decker <decker4693@gmail.com>
2019-01-31 13:16:25 -0700

    Initial commit

---
+++ src/test/java/space/straylense/cactus/CactusApplicationTests.java
@@ -0,0 +1,17 @@
+package space.straylense.cactus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CactusApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
