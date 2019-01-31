d83578c089898da62afdcac94552d1c48a7ec2f0
Travis Decker <decker4693@gmail.com>
2019-01-31 15:07:41 -0700

    Created comment Entity, Generated getters and setters.

--- src/main/java/space/straylense/cactus/model/CommentEntity.java
+++ src/main/java/space/straylense/cactus/model/CommentEntity.java
@@ -1,5 +1,57 @@
 package space.straylense.cactus.model;
 
+import java.util.Date;
+import java.util.UUID;
+import javax.persistence.Entity;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Component
 public class CommentEntity {
 
+  private UUID commentId;
+  private boolean reportFlag;
+  private UserEntity user;
+  private Date date;
+  private String comment;
+
+  public UUID getCommentId() {
+    return commentId;
+  }
+
+  public void setCommentId(UUID commentId) {
+    this.commentId = commentId;
+  }
+
+  public boolean isReportFlag() {
+    return reportFlag;
+  }
+
+  public void setReportFlag(boolean reportFlag) {
+    this.reportFlag = reportFlag;
+  }
+
+  public UserEntity getUser() {
+    return user;
+  }
+
+  public void setUser(UserEntity user) {
+    this.user = user;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  public String getComment() {
+    return comment;
+  }
+
+  public void setComment(String comment) {
+    this.comment = comment;
+  }
 }
