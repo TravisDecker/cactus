1cc46f33ab4d8f96d1f550521ac927bd24c15dd0
Travis Decker <decker4693@gmail.com>
2019-02-06 10:41:15 -0700

    Added Controller annotations, successfully posted a user.

--- src/main/java/space/straylense/cactus/controller/UserController.java
+++ src/main/java/space/straylense/cactus/controller/UserController.java
@@ -1,16 +1,26 @@
 package space.straylense.cactus.controller;
 
+import java.util.NoSuchElementException;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import space.straylense.cactus.model.UserEntity;
 import space.straylense.cactus.model.dao.UserRepository;
 
+@RestController
+@RequestMapping("/users")
+@ExposesResourceFor(UserEntity.class)
 public class UserController {
 
   private UserRepository userRepository;
@@ -26,9 +36,15 @@ public class UserController {
     return ResponseEntity.created(user.getHref()).body(user);
   }
 
-  @GetMapping(value = "{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+  @GetMapping(value = "{userId}/users", produces = MediaType.APPLICATION_JSON_VALUE)
   public UserEntity getUser(@PathVariable("userId") UUID userId) {
     return userRepository.findAllByUserId(userId);
+  }
+
+
+  @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Resource not found")
+  @ExceptionHandler(NoSuchElementException.class)
+  public void notFound() {
   }
 
 }
