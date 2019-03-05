0a0f0332fcea7fb07bf801b726bd730c7e87710d
Travis Decker <decker4693@gmail.com>
2019-03-04 19:08:46 -0700

    created PostReportPost controller method

--- src/main/java/space/straylense/cactus/controller/ReportController.java
+++ src/main/java/space/straylense/cactus/controller/ReportController.java
@@ -1,11 +1,16 @@
 package space.straylense.cactus.controller;
 
 import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import space.straylense.cactus.model.dao.PostRepository;
 import space.straylense.cactus.model.dao.ReportRepository;
 import space.straylense.cactus.model.dao.UserRepository;
+import space.straylense.cactus.model.entity.PostEntity;
 import space.straylense.cactus.model.entity.ReportEntity;
 
 @RestController
@@ -25,10 +30,12 @@ public class ReportController {
     this.reportRepository = reportRepository;
   }
 
-//@PostMapping(value = "{postId}",consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-//  public ResponseEntity<ReportEntity> postReportPost(@RequestBody ReportEntity report){
-//
-//
-//    return ResponseEntity.created(report)
-//}
+  @PostMapping(value = "{postId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<ReportEntity> postReportPost(@RequestBody ReportEntity report) {
+    PostEntity postEntity = report.getReportedPost();
+    postEntity.setReportFlag(true);
+    postRepository.save(postEntity);
+
+    return ResponseEntity.created(report.getHref()).body(report);
+  }
 }
