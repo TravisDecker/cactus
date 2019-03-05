a59867393d85597a2f3bbf1e4d8062e08dc018e6
Travis Decker <decker4693@gmail.com>
2019-03-04 19:53:35 -0700

    Corrected Report repo methods

--- src/main/java/space/straylense/cactus/model/dao/ReportRepository.java
+++ src/main/java/space/straylense/cactus/model/dao/ReportRepository.java
@@ -1,12 +1,13 @@
 package space.straylense.cactus.model.dao;
 
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 import space.straylense.cactus.model.entity.ReportEntity;
 
 public interface ReportRepository extends CrudRepository<ReportEntity, UUID> {
-//
-//  ReportEntity findAllByReportID(UUID reportId);
-//
-//  List<ReportEntity> findAllByReportReviewedOrderByReportDateDesc();
+
+  ReportEntity findAllByReportId(UUID reportId);
+
+  List<ReportEntity> findAllByReportReviewedFalse();
 }
